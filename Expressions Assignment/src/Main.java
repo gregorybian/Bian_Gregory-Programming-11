08a07f8593b80ab9fb414ce05b3f279b7805c76b
gregorybian <gregorybian100@gmail.com>
2021-11-18 21:26:32 -0800

    Expression assignment

--- Expressions Assignment/src/Main.java
+++ Expressions Assignment/src/Main.java
@@ -19,32 +19,30 @@ public class Main {
         System.out.println(5 + 10/2 - (-4)/3);
         System.out.println(5 + 10/2 - (-1));
         System.out.println(5 + 5 - (-1));
-        System.out.println(5 + 5 - (-1));
         System.out.println(10 - (-1));
         System.out.println(11);
 
         //Expression 2
         System.out.println(23%4 + (18 - 3 / 2));
         System.out.println(23%4 + (18 - 1));
-        System.out.println(23%4 + (17));
-        System.out.println(3 + (17));
-        System.out.println(3 + (17));
+        System.out.println(23%4 + 17);
+        System.out.println(3 + 17);
         System.out.println(20);
 
         //Expression 3
-        System.out.println(((8 - 5) + Math.pow(3,4)/5));
-        System.out.println(((3) + Math.pow(3,4)/5));
-        System.out.println(((3) + 81.0/5));
-        System.out.println(((3) + 16.2));
-        System.out.println((19.2));
+        System.out.println((8 - 5) + Math.pow(3,4)/5);
+        System.out.println(3 + Math.pow(3,4)/5);
+        System.out.println(3 + 81.0/5);
+        System.out.println(3 + 16.2);
+        System.out.println(19.2);
 
         //Expression 4
         System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
-        System.out.println((1) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
-        System.out.println((1) - 8 + 8.0 / 8 + (Math.sqrt(81) / 3));
-        System.out.println((1) - 8 + 1.0 + (Math.sqrt(81) / 3));
-        System.out.println((1) - 8 + 1.0 + (9.0 / 3));
-        System.out.println((1) - 8 + 1.0 + 3.0);
+        System.out.println(1 - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
+        System.out.println(1 - 8 + 8.0 / 8 + (Math.sqrt(81) / 3));
+        System.out.println(1 - 8 + 1.0 + (Math.sqrt(81) / 3));
+        System.out.println(1 - 8 + 1.0 + (9.0 / 3));
+        System.out.println(1 - 8 + 1.0 + 3.0);
         System.out.println((-7) + 1.0 + 3.0);
         System.out.println((-6.0) + 3.0);
         System.out.println(-3.0);
@@ -52,6 +50,7 @@ public class Main {
         //Expression 5
         System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
         System.out.println(((7 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
+        System.out.println((10 * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
         System.out.println((10 * 0.8509035245341184 + 2) - Math.pow(Math.sqrt(25), 3));
         System.out.println((8.509035245341184 + 2) - Math.pow(Math.sqrt(25), 3));
         System.out.println((10.509035245341184) - Math.pow(Math.sqrt(25), 3));
